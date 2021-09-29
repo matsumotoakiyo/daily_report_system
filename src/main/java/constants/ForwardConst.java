72c5f0713e119001187b2bcc70b01045d623fd74
scjpq <scjpq@LAPTOP-17MMRPSP>
2021-09-29 21:43:19 +0900

    FrontController作成

--- src/main/java/constants/ForwardConst.java
+++ src/main/java/constants/ForwardConst.java
@@ -1,37 +1,39 @@
 package constants;
 
-
 /**
  * リクエストパラメーターの変数名、変数値、jspファイルの名前等画面遷移に関わる値を定義するEnumクラス
  *
  */
 public enum ForwardConst {
 
-    // action
+    //action
     ACT("action"),
     ACT_TOP("Top"),
     ACT_EMP("Employee"),
     ACT_REP("Report"),
     ACT_AUTH("Auth"),
 
-    // command
+    //command
     CMD("command"),
     CMD_NONE(""),
     CMD_INDEX("index"),
     CMD_SHOW("show"),
-    CMD_SHOW_LOGIN("login"),
+    CMD_SHOW_LOGIN("showLogin"),
+    CMD_LOGIN("login"),
     CMD_LOGOUT("logout"),
     CMD_NEW("entryNew"),
-    CMD_CCREATE("create"),
+    CMD_CREATE("create"),
+    CMD_EDIT("edit"),
+    CMD_UPDATE("update"),
     CMD_DESTROY("destroy"),
 
-    // jsp
+    //jsp
     FW_ERR_UNKNOWN("error/unknown"),
     FW_TOP_INDEX("topPage/index"),
     FW_LOGIN("login/login"),
     FW_EMP_INDEX("employees/index"),
     FW_EMP_SHOW("employees/show"),
-    FW_EMP_NEW("employees\new"),
+    FW_EMP_NEW("employees/new"),
     FW_EMP_EDIT("employees/edit"),
     FW_REP_INDEX("reports/index"),
     FW_REP_SHOW("reports/show"),
@@ -53,7 +55,7 @@ public enum ForwardConst {
     /**
      * 値(文字列)取得
      */
-    private String getValue() {
+    public String getValue() {
         return this.text;
     }
 
